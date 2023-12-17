3f305109cab22ef3c7c21ad01c4b7c9c7e759241
Nouraalamer <153731469+Nouraalamer@users.noreply.github.com>
2023-12-17 14:14:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
-angular.module('LunchCheck', []);
 (function () {
     'use strict';
-  
+
     angular.module('LunchCheck', [])
-      .controller('LunchCheckController', LunchCheckController);
-  
+    .controller("LunchCheckController", LunchCheckController);
+
     LunchCheckController.$inject = ['$scope'];
-  
-    function LunchCheckController($scope) {
-      $scope.lunchItems = "sss";
-      $scope.message = "sss";
-  
-      $scope.checkLunch = function () {
-        var items = getNonEmptyItems($scope.lunchItems);
-  
-        if (items.length === 0) {
-          $scope.message = "Please enter data first";
-        } else if (items.length <= 3) {
-          $scope.message = "Enjoy!";
-        } else {
-          $scope.message = "Too much!";
-        }
-      };
-  
-      function getNonEmptyItems(itemsString) {
-        var items = itemsString.split(',');
-  
-        return items.filter(function (item) {
-          return item.trim() !== "sss";
-        });
-      }
+    function LunchCheckController($scope){
+        $scope.message = "";
+        $scope.color = "black";
+        $scope.tooMuch = function(){
+            if($scope.message == ""){
+                $scope.message = "please enter data first"
+            }else if($scope.message.split(",").length < 3){
+                $scope.message = "Enjoy!"
+                $scope.color = "green";
+            }else{
+                $scope.message = "Too much!"
+                $scope.color = "red";
+            }
+        };
     }
-  })();
+})();
